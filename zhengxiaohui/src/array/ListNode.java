c7404881461f8c36f60238dc740a86ec7a108c85
kaisarh <huankai7@163.com>
2021-02-04 23:20:39 +0800

    郑晓慧2020.2.3

--- zhengxiaohui/src/array/ListNode.java
+++
@@ -1,9 +0,0 @@
-package array;
-
-public class ListNode {
-    int val;//节点的值
-    ListNode next;
-    ListNode() {}
-    ListNode(int val) { this.val = val; }
-    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
-}
--- zhengxiaohui/src/array/twoSum.java
+++ zhengxiaohui/src/array/twoSum.java
@@ -1,28 +1,72 @@
 package array;
 
+
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
 
-/**
- * leetcode 1. 两数之和-哈希表
- *创建哈希表，对于每一个x，先查询哈希表中是否存在target-x，然后将x插入到哈希表中
- * 时间复杂度：O（N）
- * 空间复杂度：O（N）
- */
-class Solution1 {
-    public int[] twoSum(int[] nums, int target){
-        int len = nums.length;
-        Map<Integer,Integer>HashTable = new HashMap<>(len-1);
-        HashTable.put(nums[0], 0);
-        for (int i=1;i<len;i++){
-            int another = target-nums[i];
-            if(HashTable.containsKey(another)){
-                return new int[]{i,HashTable.get(another)};
-            }
-            HashTable.put(nums[i], i);
+// leetCode 1. 两数之和
+public class twoSum {
+    public static void main(String[] args) {
+        /*
+        * 1. 数组 int
+        * 2. 二维数组
+        * 3. 字符串
+        * 4. 树
+        * 5. 图
+        * */
+        Scanner scanner = new Scanner(System.in);
+        // nums = [2,7,11,15], target = 9
+        String a = scanner.nextLine();
+        String[] b = a.split(" ");
+        int[] c = new int[b.length];
+        for (int i = 0; i < b.length; i++) {
+            c[i] = Integer.parseInt(b[i]);
         }
-        throw new IllegalArgumentException("No Two Sum Solution!");
+        int target = scanner.nextInt();
+        int[] result = twoSumDemo3(c,target);
+        System.out.println(Arrays.toString(result));
+    }
+    public static int[] twoSumDemo1(int[] nums, int target) {
+        int[] result = new int[2];
+        for (int i = 0; i < nums.length-1; i++) {
+            for (int j = i+1; j < nums.length; j++) {
+                if(nums[i]+nums[j]==target){
+                    result[0] = i;
+                    result[1] = j;
+                }
+            }
+        }
+        return result;
+    }
+    public static int[] twoSumDemo2(int[] nums, int target) {
+        int[] result = new int[2];
+        Map<Integer,Integer> HashMap = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            HashMap.put(nums[i],i);
+        }
+        for (int j = 0; j < nums.length; j++) {
+            int another = target - nums[j];
+            if(HashMap.containsKey(another)){
+                result[0] = HashMap.get(another);
+                result[1] = j;
+            }
+        }
+        return result;
+    }
+    public static int[] twoSumDemo3(int[] nums, int target) {
+        int[] result = new int[2];
+        Map<Integer,Integer> HashMap = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int another = target - nums[i];
+            if(HashMap.containsKey(another) && HashMap.get(another) != i){
+                result[0] = HashMap.get(another);
+                result[1] = i;
+                break;
+            }
+            HashMap.put(nums[i],i);
+        }
+        return result;
     }
 }
